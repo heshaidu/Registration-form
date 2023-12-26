cba22abdecbd340f622546ef937c155dc225bca9
Heshani Waduge <132739243+heshaidu@users.noreply.github.com>
2023-12-27 00:40:44 +0530

    Add files via upload

---
+++ applicationsform.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration Form</title>
+</head>
+<body>
+
+<p><h1>Registration Form</p></h1>
+
+<form method="post" action="registration_form.php">
+	<label for="firstName">First Name</label>
+	<input type="text" name="firstName" id="fName"><br>
+
+	<label for="lastName">Last Name</label>
+	<input type="text" name="lastName" id="lName"><br>
+
+	<input type="hidden" name="form_submited" value="1">
+	<input type="submi" value="Submit">
+</form>
+</body>
+</html>
